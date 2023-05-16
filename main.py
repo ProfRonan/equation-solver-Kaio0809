c6f89982f7ba8e13b56f5ff36483067e8fbfc2b5
Kaio <128155244+Kaio0809@users.noreply.github.com>
2023-05-16 12:03:09 +0000

    exercicio concluído

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-grau= int(input("Escreva o grau da equação: "))
+grau = int(input("Escreva o grau da equação: "))
 if 1>grau or grau>2:
     print('Grau inválido')
 else:
@@ -19,7 +19,8 @@ else:
         else: 
             b = int(input('Digite o valor de b: '))
             c = int(input('Digite o valor de c: '))
-            delta = b**2 - 4*a*c
+            delta = (b**2) - (4*a*c)
+            raiz = delta**0.5
             if delta < 0:
                 print('A equação não possui raízes reais')
             elif delta == 0:
@@ -28,7 +29,7 @@ else:
                 print(f'x = {x:.2f}')
             elif delta > 0: 
                 print('A equação possui duas raízes reais')
-                x1 = (-b + delta**0.5)/2*a
-                x2 = (-b - delta**0.5)/2*a
+                x1 = (-b + raiz)/(2*a)
+                x2 = (-b - raiz)/(2*a)
                 print(f'x1 = {x1:.2f}')
                 print(f'x2 = {x2:.2f}')
